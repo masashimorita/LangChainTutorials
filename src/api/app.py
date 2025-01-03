663eb0554774b7f3f13a705216d149f862ce8ee7
masashimorita <masashi.morita.mm@gmail.com>
2025-01-03 14:04:01 +0900

    feat: :sparkles: Implement IceBreaker with LinkedIn profile

--- src/api/app.py
+++ src/api/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from .ice_breaker import ice_break_with_linkedin
+
 # Define FastAPI app
 app = FastAPI()
 
@@ -7,3 +9,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/ice_breaker/linkedin")
+def ice_breaker_linkedin():
+    return ice_break_with_linkedin()
---
+++ src/api/ice_breaker.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+from src.third_parties.linkedin import scrape_linkedin_profile
+
+
+load_dotenv();
+
+def ice_break_with_linkedin():
+    print("Hello LangChain!")
+
+    summary_tempalte = """
+        given the LinkedIn information {information} about a person from I want you to create:
+        1. a short summary
+        2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_tempalte
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = summary_prompt_template | llm | StrOutputParser()
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/eden-marco/", mock=True)
+
+    res = chain.invoke(input={"information": linkedin_data})
+
+    return res
